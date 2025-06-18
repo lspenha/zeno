5ccfcb28ebd6f3033658389d00bc50c3b8e9f1c6
Lucas Penha <lucas.penha@actdigital.com>
2025-06-18 13:25:11 -0300

    feat: update

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,17 +1,12 @@
+"use client"
 export interface ButtonProps {
-  /** Is this the principal call to action on the page? */
   primary?: boolean;
-  /** What background color to use (Tailwind class override) */
   backgroundColor?: string;
-  /** How large should the button be? */
   size?: 'small' | 'medium' | 'large';
-  /** Button contents */
   label: string;
-  /** Optional click handler */
   onClick?: () => void;
 }
 
-/** Primary UI component for user interaction */
 export const Button = ({
   primary = false,
   size = 'medium',
